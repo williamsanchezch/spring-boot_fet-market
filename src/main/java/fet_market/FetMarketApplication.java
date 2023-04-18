a798686275f8e4f3a1c5d86e0b1aa52806d5245b
williamsanchezch <william_sanchezch@fet.edu.co>
2023-04-17 20:50:10 -0500

    Firts commit

---
+++ src/main/java/fet_market/FetMarketApplication.java
@@ -0,0 +1,13 @@
+package fet_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fet_market/FetMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package fet_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
